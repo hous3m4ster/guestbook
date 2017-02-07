2bd8e8b0bbf159caab87e70d9c1ced10968d71ae
hous3m4ster <hous3m4ster@protonmail.com>
2017-02-07 14:02:27 +0100

    added a comment with details

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Easy Guestbook with database
+by hous3m4ster
+
+for questions, contact me on my telegram (@hous3m4ster) or send me an email (hous3m4ster@outlook.com)
+*/
+include("required/core.php");
+
+ ?>
--- required/core.php
+++ required/core.php
@@ -1,3 +1,9 @@
 <?php
+/*
+Easy Guestbook with database
+by hous3m4ster
+
+for questions, contact me on my telegram (@hous3m4ster) or send me an email (hous3m4ster@outlook.com)
+*/
 //TODO
  ?>
